d549b930fe9faa40790995b19faec8ebe966dca3
happyfrogg <157680327+happyfrogg@users.noreply.github.com>
2025-10-22 11:05:10 +0900

    add static

--- django_docker/settings.py
+++ django_docker/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["localhost", "13.209.53.114"]
 
 # Application definition
 
